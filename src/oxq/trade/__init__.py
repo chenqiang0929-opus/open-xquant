5129c98ba85954990d93a06336b3543111a99e96
xingwudao <kaijiangchen@gmail.com>
2026-03-11 10:47:20 +0800

    feat(trade): export LiveBroker and AlpacaClient from trade module

--- src/oxq/trade/__init__.py
+++ src/oxq/trade/__init__.py
@@ -9,3 +9,12 @@ __all__ = [
     "SimBroker",
     "SlippageModel",
 ]
+
+# LiveBroker is only available when httpx + websockets are installed
+try:
+    from oxq.trade.alpaca_client import AlpacaAPIError, AlpacaClient
+    from oxq.trade.live_broker import LiveBroker
+
+    __all__ += ["AlpacaAPIError", "AlpacaClient", "LiveBroker"]
+except ImportError:
+    pass
